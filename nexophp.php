8dbc559787e6e41e5b5c143ead34afca6589f1da
ken <68103403@qq.com>
2025-07-23 15:35:02 +0800

    init

--- nexophp.php
+++ nexophp.php
@@ -7,10 +7,14 @@ $module_info = [
 	'version' => '1.0.2', 
 	'title' => '测试模块',
 	'description' => '这是一个测试模块', 
-	'url' => 'https://www.google.com',
+	'url' => '',
 	'email' => '68103403@qq.com',
 	'author' => 'sunkangchina'
 ];
 
+
+add_action("header_center",function(){
+	echo 'demo_module';
+});
 
 
